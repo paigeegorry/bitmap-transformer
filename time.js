518f0764b9ef38dfbc4eec727aad82c37b5fc38d
Paige Gorry <paigeegorry@gmail.com>
2019-01-10 09:32:21 -0800

    settimeout exercise in class

---
+++ time.js
@@ -0,0 +1,8 @@
+// setTimeout(() => {
+//   console.log('OHHHHH, callbacks?');
+//   setTimeout(() => {
+//     console.log('one second later');
+//   }, 1000);
+// }, 2000);
+
+setInterval(console.log, 1000, 'hello!');
